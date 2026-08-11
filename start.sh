a3e719acdfd4dd193b39e64afa5708bc66b39eae
rashelmdv <juan2005may@gmail.com>
2026-08-11 12:34:33 -0600

    Corregido script de inicio para Render

--- start.sh
+++ start.sh
@@ -3,6 +3,10 @@
 # Iniciar Nginx en segundo plano
 nginx -g "daemon off;" &
 
+# Esperar 2 segundos para que Nginx arranque
+sleep 2
+
 # Iniciar el backend
+echo "Iniciando backend..."
 cd /app/backend
 node src/app.js
